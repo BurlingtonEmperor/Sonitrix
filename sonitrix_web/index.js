e443508c5a8e54de72dc2e16221491ceb37aa9b4
Alexander Chang <alexanderchang208@gmail.com>
2026-05-31 13:32:22 -0500

    update sonitrix web

--- sonitrix_web/index.js
+++ sonitrix_web/index.js
@@ -1,14 +1,67 @@
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-const oscillator = audioCtx.createOscillator();
+// const oscillator = audioCtx.createOscillator();
 const gainNode = audioCtx.createGain();
 
-oscillator.type = 'sine';            
-oscillator.frequency.value = 19500;  
+// oscillator.type = 'sine';            
+// oscillator.frequency.value = 19500;  
 
-gainNode.gain.value = 1.0; 
+// gainNode.gain.value = 1.0; 
 
-oscillator.connect(gainNode);
-gainNode.connect(audioCtx.destination);
+// oscillator.connect(gainNode);
+// gainNode.connect(audioCtx.destination);
 
-oscillator.start();
+let oscillator = null;
+let default_frequency = 19500;
+
+function startTone (frequency) {
+  oscillator = audioCtx.createOscillator();
+  oscillator.type = 'sine';
+  oscillator.frequency.value = parseInt(frequency);
+
+  oscillator.connect(audioCtx.destination);
+  oscillator.start();
+}
+
+function stopTone() {
+  if (oscillator) {
+    oscillator.stop(); 
+    oscillator = null; 
+  }
+}
+
+// interface
+const default_button = document.getElementById("default-selection");
+const stop_emission = document.getElementById("stop-emission");
+
+// let has_played = 0;
+let is_playing = 0;
+
+default_button.onclick = function () {
+  switch (is_playing) {
+    case 1:
+      return false;
+  }
+
+  is_playing = 1;
+  startTone(default_frequency);
+
+//   switch (has_played) {
+//     case 0:
+//       has_played = 1;
+//       is_playing = 1;
+//       startTone(default_frequency);
+//       break;
+//     case 1:
+//       break;
+//   }
+}
+
+stop_emission.onclick = function () {
+  switch (is_playing) {
+    case 1:
+      stopTone();
+      is_playing = 0;
+      break;
+  }
+}
